db3b2b355033501b8c285b191970fb5f807b9e91
HafidzSuhaiby <79145498+HafidzSuhaiby@users.noreply.github.com>
2026-07-19 14:37:45 +0700

    Fix PW

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -44,14 +44,8 @@ class AppServiceProvider extends ServiceProvider
             app()->isProduction(),
         );
 
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
-                ->mixedCase()
-                ->letters()
-                ->numbers()
-                ->symbols()
-                ->uncompromised()
-            : null,
-        );
+        Password::defaults(function () {
+            return Password::min(8);
+        });
     }
 }
